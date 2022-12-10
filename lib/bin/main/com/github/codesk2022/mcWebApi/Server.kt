4630f47ace657e5db5243ef28db19470d1a1abdf
ikasoba <57828948+ikasoba@users.noreply.github.com>
2022-12-10 13:21:10 +0900

    unko

--- lib/bin/main/com/github/codesk2022/mcWebApi/Server.kt
+++ lib/bin/main/com/github/codesk2022/mcWebApi/Server.kt
@@ -5,4 +5,12 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import com.github.codesk2022.mcWebApi.web.chat
 
+public fun server(plugin: Plugin): NettyApplicationEngine {
+  return embeddedServer(Netty) {
+    routing {
+      chat(this, plugin)
+    }
+  }
+}
